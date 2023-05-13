077111691d06dfeb264073c3c95c7ee43759dc4f
Alphonso Sensley II <alphonso.sensleyjr@gmail.com>
2023-05-13 10:32:47 -0400

    Initial Commit

---
+++ SwiftyHelper/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SwiftyHelper
+//
+//  Created by Alphonso Sensley II on 5/13/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftyHelper/SwiftyHelperApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftyHelperApp.swift
+//  SwiftyHelper
+//
+//  Created by Alphonso Sensley II on 5/13/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftyHelperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
